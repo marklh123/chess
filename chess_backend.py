ec58a6b4012f8123804c0774459686ff62feee57
markh <mark.tennis.king@gmail.com>
2026-04-26 21:30:40 -0400

    e

--- chess_backend.py
+++ chess_backend.py
@@ -430,7 +430,6 @@ def PawnPromotion(piece,new_row,col):
 
     blackbackranks = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7)]
     whitebackranks = [(7, 0), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7)]
-    # back rank pos for white: (row 7)
 
     if piece["color"] == "white" and piece["type"] == "pawn":
         if (new_row, letter_index[col]) in blackbackranks:
